5fc32ec10ec83d6902a5c81b92a1f3888f8e7b33
Arslan jafar <arslanbinjaffar12000@gmail.com>
2024-01-06 13:58:45 +0500

    changes in api2

--- routes/Propermessage.js
+++ routes/Propermessage.js
@@ -2,13 +2,13 @@ import  { upload } from "../utils/MulterStorage.js"
 import  { Router } from "express"
 
 // import  {uploadFile,sendMessage,bulkMessage,getGroups,uploadGroups} from "../controllers/_messages.js"
-import  {getGroups} from "../controllers/_messages.js"
+import  {getGroups,uploadFile} from "../controllers/_messages.js"
 
 const router = Router()
 
 
 router.get('/getallgroups',getGroups)
-// router.post('/upload',uploadFile)
+router.post('/upload',uploadFile)
 // router.post('/sendmessage',sendMessage)
 // router.post('/upload',uploadGroups)
 // router.post('/sendbulkmessages',bulkMessage)
